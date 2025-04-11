a55fb25b301c0a5b7c9aab40abbb6357a3b80ef7
Noone <kasraam385@gmail.com>
2025-04-11 23:14:05 +0430

    test2 ...

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,8 @@ export default function Loading() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <AiOutlineLoading3Quarters className="animate-spin text-8xl text-zinc-500" />
-      {/* add this to redeploy */}
+      {/* add this to redeploy
+      github work correct now test google */}
     </div>
   );
 }
