9658de4ef5deb90058b2106803d2859c8b30672c
Noone <kasraam385@gmail.com>
2025-04-11 23:02:35 +0430

    auth.js secret added

--- src/auth.js
+++ src/auth.js
@@ -4,4 +4,5 @@ import Github from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google, Github],
+  secret: process.env.NEXTAUTH_SECRET,
 });
